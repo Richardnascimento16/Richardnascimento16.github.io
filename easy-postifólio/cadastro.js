015c61a032fbf372070da7b060d23452e82b2f4e
Fair House <richardnascimentosousa16@gmail.com>
2025-03-28 17:18:24 -0300

    atualização

--- easy-postifólio/cadastro.js
+++ easy-postifólio/cadastro.js
@@ -1,14 +1,21 @@
-function required(){
-    var email=document.getElementById("email_e");
-    var senha=document.getElementById("senha_s");
+function required() {
+    var email = document.getElementById("email_e");
+    var senha = document.getElementById("senha_s");
 
-    if(email.value=="" && senha.value==""){
-        alert("campo EMAIL e SENHA vazio");
-    }else if(email.value==""){
-        alert("campo EMAIL vazio");
-    }else if(senha.value==""){
-        alert("campo SENHA vazio");
-    }else{
-        window.location.href= "home.html"
+    // Verificando se ambos os campos estão vazios
+    if (email.value == "" && senha.value == "") {
+        alert("Campos EMAIL e SENHA estão vazios.");
+    } 
+    // Verificando se o campo EMAIL está vazio
+    else if (email.value == "") {
+        alert("Campo EMAIL está vazio.");
+    } 
+    // Verificando se o campo SENHA está vazio
+    else if (senha.value == "") {
+        alert("Campo SENHA está vazio.");
+    } 
+    else {
+        // Se ambos os campos forem preenchidos, redireciona para a página home.html
+        window.location.href = "home.html";
     }
 }
