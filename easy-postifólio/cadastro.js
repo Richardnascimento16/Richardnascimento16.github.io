10430f9ad4541cc91412c59c368d0f11feeb20dd
Fair House <richardnascimentosousa16@gmail.com>
2025-04-02 14:54:00 -0300

    update cadastro.js

--- easy-postifólio/cadastro.js
+++ easy-postifólio/cadastro.js
@@ -1,33 +1,17 @@
 function required(event) {
     var email = document.getElementById("email_e");
     var senha = document.getElementById("senha_s");
-    var valid = true;
 
-    // Resetando estilos anteriores
-    email.classList.remove("is-invalid");
-    senha.classList.remove("is-invalid");
-
-    if (email.value.trim() === "" && senha.value.trim() === "") {
+    if (email.value=== "" && senha.value=== "") {
         alert("Campos EMAIL e SENHA estão vazios.");
-        email.classList.add("is-invalid");
-        senha.classList.add("is-invalid");
-        valid = false;
     } 
-    else if (email.value.trim() === "") {
+    else if (email.value=== "") {
         alert("Campo EMAIL está vazio.");
-        email.classList.add("is-invalid");
-        valid = false;
     } 
-    else if (senha.value.trim() === "") {
+    else if (senha.value === "") {
         alert("Campo SENHA está vazio.");
-        senha.classList.add("is-invalid");
-        valid = false;
-    } 
-
-    if (!valid) {
-        event.preventDefault(); // Impede o envio do formulário
-        return false;
+    }else{
+        window.location="home.html"
     }
-    
-    return true; // Permite o envio do formulário
+
 }
